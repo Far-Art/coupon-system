a1f360834eeb52fc714656865b2b46804d78a308
Artur Farmanov <arturfarmanov91@gmail.com>
2021-09-28 13:30:17 +0300

    Improved routing, no more unauthorized routing through tampering with url, now routing allowed only through components

--- src/Components/LayoutArea/Routing/Routing.tsx
+++ src/Components/LayoutArea/Routing/Routing.tsx
@@ -4,16 +4,12 @@ import { ClientType } from "../../../Models/ClientType";
 import { useAppSelector } from "../../../Redux/Hooks/hooks";
 import { RouteUrls } from "../../../Services/RouteUrls";
 import Cart from "../Cart/Cart";
-import CouponsContainer from "../../CouponsArea/CouponsContainer/CouponsContainer";
 import CreateCompanyForm from "../../InputArea/CreateCompanyForm/CreateCompanyForm";
 import CreateCouponForm from "../../InputArea/CreateCouponForm/CreateCouponForm";
 import CreateCustomerForm from "../../InputArea/CreateCustomerForm/CreateCustomerForm";
 import EmptyView from "../../SharedArea/EmptyView/EmptyView";
-import Login from "../Login/Login";
-import Logout from "../Logout/Logout";
 import MainView from "../MainView/MainView";
 import ProfileView from "../ProfileView/ProfileView";
-import Sign_up from "../Sign_up/Sign_up";
 import CreditsPage from "../CreditsPage/CreditsPage";
 
 export default function Routing(): JSX.Element {
@@ -25,13 +21,8 @@ export default function Routing(): JSX.Element {
         <div className="Routing">
             <Switch>
                 <Route path={RouteUrls.HOME} component={MainView} exact />
-                <Route path={RouteUrls.COUPONS} component={CouponsContainer} exact />
-                <Route path={RouteUrls.COUPONS + ":id"} component={CouponsContainer} exact />
-                <Route path={RouteUrls.LOGIN} component={Login} exact />
-                <Route path={RouteUrls.SIGNUP} component={Sign_up} exact />
-                <Route path={RouteUrls.LOGOUT} component={Logout} exact />
-                <Route path={RouteUrls.PROFILE} component={ProfileView} exact />
                 <Route path={"/credits"} component={CreditsPage} exact />
+                {client !== undefined && <Route path={RouteUrls.PROFILE} component={ProfileView} exact />}
                 {(client === undefined || client?.clientType === ClientType.CUSTOMER) && <Route path={RouteUrls.CART} component={Cart} exact />}
                 {(client as ClientInfoModel)?.active && client?.clientType === ClientType.COMPANY && <Route path={RouteUrls.CREATE_COUPON} component={CreateCouponForm} exact />}
                 {client?.clientType === ClientType.ADMIN && <Route path={RouteUrls.CREATE_COMPANY} component={CreateCompanyForm} exact />}
