ab729dac76f5ddac99e22f68e62cb880f53b4d8a
Artur Farmanov <arturfarmanov91@gmail.com>
2021-10-22 12:04:38 +0300

    separated documantation and credits pages

--- src/Components/LayoutArea/Footer/Footer.tsx
+++ src/Components/LayoutArea/Footer/Footer.tsx
@@ -5,7 +5,8 @@ export default function Footer(): JSX.Element {
     return (
         <div className="Footer WHITE__BG">
                 <a href="https://far-art-portfolio.herokuapp.com/" target="_blank" rel="noreferrer">This coupon system mockup is a personal project of Artur Farmanov&copy; and is made only for skills demonstration.</a>
-                <NavLink to="/credits">To documentation and credits page</NavLink>
+                <NavLink to="/credits">To credits page</NavLink>
+                <NavLink to="/documentation">To full documentation page</NavLink>
         </div>
     );
 }
