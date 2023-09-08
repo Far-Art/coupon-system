c456b37836c63e78879d3f02cc88dfa48226c06b
far-art <arturfarmanov91@gmail.com>
2023-09-08 15:53:14 +0300

    added demo login buttons for easy project exploration

--- src/Components/LayoutArea/Login/Login.tsx
+++ src/Components/LayoutArea/Login/Login.tsx
@@ -1,5 +1,5 @@
-import { useForm } from "react-hook-form";
-import { LoginRequestModel } from "../../../Models/LoginRequestModel";
+import {useForm} from "react-hook-form";
+import {LoginRequestModel} from "../../../Models/LoginRequestModel";
 import "./Login.css";
 import ApiGlobalLogic from "../../../Services/ApiGlobalLogic";
 import GlobalDataStreamer from "../../../Services/GlobalDataStreamer";
@@ -7,7 +7,7 @@ import IdleTimerApi from "../../../Services/IdleTimerApi";
 
 export default function Login(): JSX.Element {
 
-    const { register, handleSubmit, reset, formState: { errors } } = useForm<LoginRequestModel>();
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<LoginRequestModel>();
 
     const send = (login: LoginRequestModel) => {
 
@@ -19,14 +19,32 @@ export default function Login(): JSX.Element {
         });
     }
 
+    const demoLogin = (type: 'ADMIN' | 'COMPANY' | 'CUSTOMER') => {
+        let login: LoginRequestModel;
+        switch (type) {
+            case "ADMIN":
+                login = {email: 'demo@admin.com', password: 'Admin123'};
+                break;
+            case "COMPANY":
+                login = {email: 'demo@company.com', password: 'Company123'};
+                break;
+            case "CUSTOMER":
+                login = {email: 'demo@customer.com', password: 'Customer123'};
+                break;
+        }
+
+        send(login);
+    }
+
     return (
         <section className="Login">
             <div className="Login__Form FORM">
                 <h2> Login </h2>
                 <form onSubmit={handleSubmit(send)}>
 
-                    <input maxLength={ApiGlobalLogic.forms.fieldsMaxLength.email} type="email" className="FIELD" placeholder="email" {...register("email", {
-                        required: { value: true, message: "Email required" },
+                    <input maxLength={ApiGlobalLogic.forms.fieldsMaxLength.email} type="email" className="FIELD"
+                           placeholder="email" {...register("email", {
+                        required: {value: true, message: "Email required"},
                         minLength: {
                             value: ApiGlobalLogic.forms.fieldsMinLength.email,
                             message: ApiGlobalLogic.errorDescriptions.minLength.email
@@ -38,20 +56,26 @@ export default function Login(): JSX.Element {
                     })} />
                     {errors.email && <p className="Error">{errors.email.message}</p>}
 
-                    <input maxLength={ApiGlobalLogic.forms.fieldsMaxLength.password} type="password" className="FIELD" placeholder="password" {...register("password", {
-                        required: { value: true, message: "Password required" }
-                    })}
+                    <input maxLength={ApiGlobalLogic.forms.fieldsMaxLength.password} type="password" className="FIELD"
+                           placeholder="password" {...register("password", {
+                        required: {value: true, message: "Password required"}
+                        })}
                     />
                     {errors.password && <p className="Error">{errors.password.message}</p>}
 
-                    <br />
+                    <br/>
                     <button type="reset" onClick={() => reset()} className="BUTTON__AS_LINK">clear fields</button>
-                    <br />
+                    <br/>
 
                     <div>
                         <button type="submit" className="APP__BUTTON">Login</button>
                     </div>
                 </form>
+                <br/>
+                <div>demo login</div>
+                <button onClick={() => demoLogin('CUSTOMER')} className="APP__BUTTON">Login as customer</button>
+                <button onClick={() => demoLogin('COMPANY')} className="APP__BUTTON">Login as company</button>
+                <button onClick={() => demoLogin('ADMIN')} className="APP__BUTTON">Login as admin</button>
             </div>
         </section>
     );
